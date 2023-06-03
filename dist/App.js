17303f20909833218597b9eb24c8676edd73181a
andy19910102 <andy19910102@gmail.com>
2023-06-03 22:18:11 +0800

    Consider use babel for build

---
+++ dist/App.js
@@ -0,0 +1,32 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+var _react = _interopRequireDefault(require("react"));
+var _Donut = _interopRequireDefault(require("./Donut/Donut"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+function App() {
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "App",
+    style: {
+      display: 'flex',
+      flexWrap: 'wrap'
+    }
+  }, /*#__PURE__*/_react.default.createElement(_Donut.default, {
+    color: "#dd0000",
+    scaleX: 1,
+    scaleY: 0.75
+  }), /*#__PURE__*/_react.default.createElement(_Donut.default, {
+    color: "#8E2DE2",
+    scaleX: 1,
+    scaleY: 0.75
+  }), /*#__PURE__*/_react.default.createElement(_Donut.default, {
+    color: "#11998e",
+    scaleX: 1,
+    scaleY: 0.75
+  }));
+}
+var _default = App;
+exports.default = _default;
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+var _client = _interopRequireDefault(require("react-dom/client"));
+var _App = _interopRequireDefault(require("./App"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var root = _client.default.createRoot(document.getElementById('root'));
+root.render( /*#__PURE__*/_react.default.createElement(_react.default.StrictMode, null, /*#__PURE__*/_react.default.createElement(_App.default, null)));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
